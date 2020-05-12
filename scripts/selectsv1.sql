ab7d6f2fbfb5c9f561f37f6b701e96dce352c7e9
Carlos Martinez <martinez96carlos@gmail.com>
2020-05-12 00:22:25 -0400

    una consulta mas para los pedidos

--- scripts/selectsv1.sql
+++ scripts/selectsv1.sql
@@ -4,5 +4,7 @@ from solids A, solid_type B
 where A.solid_type_id = B.solid_type_id;
 
 
-
-
+/* Mostrar el nombre del recolector, del generador y la fecha del pedido*/
+select a.recolector_first_name recolector, b.generator_first_name generador_nombre,b.generator_second_lastname generador_apellido, b.generator_place lugar, c.order_date
+from recolectors a, generators b, orders c
+where a.recolector_id = c.recolector_id and b.generator_id = c.generator_id;
