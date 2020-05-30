2f08220d6df5d3556804cb98c753afa04ef76bdd
Carlos Martinez <martinez96carlos@gmail.com>
2020-05-30 00:22:35 -0400

    base de datos final

--- scripts/db1insert1.sql
+++ scripts/db1insert1.sql
@@ -26,28 +26,32 @@ VALUES
 ('Papel y Carton', 'Todo tipo papel, periódico, cartones y otros.', 1),
 ('Vidrio','Todas las botellas de vidrio, de cervezas, refrescos, gaseosas.', 1),
 ('Plastico', 'Bolsas de yogurt, botellas de plastico , bidones, bolsas.', 1),
-('Metales', 'Latas de Aluminio, ollas, sartenes, toda la chatarra.', 1);
+('Metales', 'Latas de Aluminio, ollas, sartenes, toda la chatarra.', 1),
+('Otros', 'Aceites, yute, goma, liquidos', 1);
 
 
 
 /*  Insertar residuos Sólidos*/
 INSERT INTO solids (solid_type_id, solid_name, solid_description, solid_state) 
 VALUES
-(1,'Perdiodico','Papel de noticias que venden en las esquinas de los vecindarios.' ,1),
+(1,'Papel Perdiodico','Papel de noticias que venden en las esquinas de los vecindarios.' ,1),
 (1,'Papel Blanco','Papel de impresión, bond, papel de oficina.',1),
 (1,'Papel Mixto','Papel bond reciclado, con diferentes elementos.',1),
 (1,'Carton','Envolturas de electrodomésticos, cajas para guardar elementos similares.',1),
 (1,'Cartulina','Hoja un poco mas gruesa generalmente usada para presentaciones, tarjetas.',1),
-(2,'Blanco','Este tipo de vidio utilizado en oficinas, no es translúcido.',1),
-(2,'De color','Botellas de refrescos, jugos, cerveza.',1),
+(2,'Vidrio Blanco','Este tipo de vidio utilizado en oficinas, no es translúcido.',1),
+(2,'Vidrio De color','Botellas de refrescos, jugos, cerveza.',1),
 (3,'PET','Botellas plásticas desechables por ejemplo.',1),
-(3,'Duro','Plasticos de juguetes, de utensilios rotos.',1),
-(3,'Blanco','Plastico para forrar cuadernos, nylon',1),
+(3,'Plastico Duro','Plasticos de juguetes, de utensilios rotos.',1),
+(3,'Plastico Blanco','Plastico para forrar cuadernos, nylon',1),
 (3,'Bolsas Termoresistentes','Bolsas para guardar alimentos o sandwiches.',1),
 (3,'Bolsas','Bolsas plasticas de basura comunes.',1),
 (4,'Latas de Aluminio',' Latas de leche condensada, champiñones por ejemplo, verduras enlatadas, latas de cerveza.',1),
 (4,'Ollas de Aluminio',' Diferentes ollas para cocinar, metal más rígido.',1),
-(4,'Chatarra u Olajata','Restos de computadoras, electrodomésticos.',1);
+(4,'Chatarra u Olajata','Restos de computadoras, electrodomésticos.',1),
+(5,'Aceites', 'Otro tipo de residuos',1),
+(5,'Gomas, caucho', 'Otro tipo de residuos',1),
+(5,'Liquidos', 'Detergente, jabon',1);
 
 INSERT INTO generators
 (
@@ -99,9 +103,9 @@ VALUES
 /* insertar pedidos */ 
 insert into orders(generator_id, recolector_id, order_date, order_detail, order_image_url, order_rate,order_recolection_rate, order_latitude, order_longitude, order_state)
 values
-(1, 2,'2020-05-01 11:11','Tengo papeles de oficina', '',0.0, 0.0,'-16.546613', '-68.075107',2),
-(2, 1,'2020-05-02 12:12','Tengo botellas de vidrio', '',0.0, 0.0,'-16.546469', '-68.074206',2),
-(1, null,'2020-05-03 13:13','Tengo botellas de vidrio', '',0.0, 0.0,'-16.546312', '-68.074206',0);
+(1, 2,'2020-05-01 11:11:00','Tengo papeles de oficina', '',4, 5,'-16.546613', '-68.075107',2),
+(1, 3,'2020-05-02 12:12:00','Tengo botellas de vidrio', '',4, 4,'-16.546469', '-68.074206',2),
+(1, null,'2020-05-03 13:13:00','Tengo botellas de vidrio', '',0.0, 0.0,'-16.546312', '-68.074206',0);
 
 /* insertar recolecciones */
 insert into recolections (solid_id , order_id, recolection_weight, recolection_state)
@@ -120,3 +124,15 @@ $2b$10$EK0.DTWLnNzsBRru9LGBDOu1d95qzRQaBnNx3MZbesbiukoj1QUlG
 Password recolector 1
 $2b$10$Dgf./jH24fNTsiyiy.8rMOchbXm.ZTUTxd/hw2JnYTpNModFgnedm
 $2b$10$Z2eqhLQan3dJUCtXmHfNV.0hIqxTY0f4P51.JaLun2l4ekHSCS066*/
+
+
+
+
+/*Base de datos para pruebas*/
+const pool = new Pool({
+    host: 'ec2-34-195-169-25.compute-1.amazonaws.com',
+    database: 'd5jlqojdg7lrhm',
+    user: 'fdevkbaiodubzx',
+    password: 'a03def1fcadb6bcb7cb110b34b0e2e1d89c9e3912872df164b2302bd6d4c974a',
+    port: '5432'
+});
---
+++ scripts/dm5.sql
@@ -0,0 +1,116 @@
+/*==============================================================*/
+/* DBMS name:      PostgreSQL 8                                 */
+/* Created on:     5/29/2020 19:08:21                           */
+/*==============================================================*/
+
+
+drop table DIM_ORDERS;
+
+drop table DIM_RECOLECTOR;
+
+drop table DIM_SOLIDS;
+
+drop table DIM_SOLID_TYPES;
+
+drop table DIM_TIME;
+
+drop table FACT_RECOLECTION;
+
+/*==============================================================*/
+/* Table: DIM_ORDERS                                            */
+/*==============================================================*/
+create table DIM_ORDERS (
+   ORDER_ID             int4                 not null,
+   RECOLECTOR_ID        int4                 null,
+   ORDER_DATE           varchar(40)          null,
+   ORDER_RATE           float8               null,
+   ORDER_RECOLECTION_RATE float8               null,
+   ORDER_LATITUDE       varchar(50)          null,
+   ORDER_LONGITUDE      varchar(50)          null,
+   GENERATOR_PLACE      varchar(50)          null,
+   constraint PK_DIM_ORDERS primary key (ORDER_ID)
+);
+
+/*==============================================================*/
+/* Table: DIM_RECOLECTOR                                        */
+/*==============================================================*/
+create table DIM_RECOLECTOR (
+   RECOLECTOR_ID        int4                 not null,
+   NOMBRE               varchar(50)          null,
+   APELLIDO             varchar(50)          null,
+   BORN_DATE            varchar(50)          null,
+   GENDER               varchar(50)          null,
+   CITY                 varchar(50)          null,
+   RATE                 float8               null,
+   constraint PK_DIM_RECOLECTOR primary key (RECOLECTOR_ID)
+);
+
+/*==============================================================*/
+/* Table: DIM_SOLIDS                                            */
+/*==============================================================*/
+create table DIM_SOLIDS (
+   SOLID_ID             int4                 not null,
+   SOLID_TYPE_ID        int4                 null,
+   SOLID_NAME           varchar(120)         null,
+   constraint PK_DIM_SOLIDS primary key (SOLID_ID)
+);
+
+/*==============================================================*/
+/* Table: DIM_SOLID_TYPES                                       */
+/*==============================================================*/
+create table DIM_SOLID_TYPES (
+   SOLID_TYPE_ID        int4                 not null,
+   SOLID_TYPE_NAME      varchar(50)          null,
+   constraint PK_DIM_SOLID_TYPES primary key (SOLID_TYPE_ID)
+);
+
+/*==============================================================*/
+/* Table: DIM_TIME                                              */
+/*==============================================================*/
+create table DIM_TIME (
+   TIME_ID              int4                 not null,
+   ANO                  varchar(40)          null,
+   MES                  varchar(40)          null,
+   DIA                  varchar(40)          null,
+   HORA                 varchar(40)          null,
+   TIME_DATE            varchar(40)          null,
+   constraint PK_DIM_TIME primary key (TIME_ID)
+);
+
+/*==============================================================*/
+/* Table: FACT_RECOLECTION                                      */
+/*==============================================================*/
+create table FACT_RECOLECTION (
+   RECOLECTION_ID       int4                 not null,
+   SOLID_ID             int4                 null,
+   ORDER_ID             int4                 null,
+   TIME_ID              int4                 null,
+   WEIGHT               float8               null,
+   constraint PK_FACT_RECOLECTION primary key (RECOLECTION_ID)
+);
+
+alter table DIM_ORDERS
+   add constraint FK_DIM_ORDE_REFERENCE_DIM_RECO foreign key (RECOLECTOR_ID)
+      references DIM_RECOLECTOR (RECOLECTOR_ID)
+      on delete restrict on update restrict;
+
+alter table DIM_SOLIDS
+   add constraint FK_DIM_SOLI_REFERENCE_DIM_SOLI foreign key (SOLID_TYPE_ID)
+      references DIM_SOLID_TYPES (SOLID_TYPE_ID)
+      on delete restrict on update restrict;
+
+alter table FACT_RECOLECTION
+   add constraint FK_FACT_REC_REFERENCE_DIM_SOLI foreign key (SOLID_ID)
+      references DIM_SOLIDS (SOLID_ID)
+      on delete restrict on update restrict;
+
+alter table FACT_RECOLECTION
+   add constraint FK_FACT_REC_REFERENCE_DIM_ORDE foreign key (ORDER_ID)
+      references DIM_ORDERS (ORDER_ID)
+      on delete restrict on update restrict;
+
+alter table FACT_RECOLECTION
+   add constraint FK_FACT_REC_REFERENCE_DIM_TIME foreign key (TIME_ID)
+      references DIM_TIME (TIME_ID)
+      on delete restrict on update restrict;
+
